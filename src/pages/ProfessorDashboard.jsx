eec7beb23be63ca08110a444cc04eb69fb2b6604
shreeraj-chatterjee <shreerajchatterjee@outlook.com>
2026-08-11 02:26:06 +0530

    Fix syntax error

--- src/pages/ProfessorDashboard.jsx
+++ src/pages/ProfessorDashboard.jsx
@@ -85,7 +85,7 @@ export default function ProfessorDashboard() {
                   </div>
                 </div>
               </div>
-            )}
+            ))}
           </div>
         </div>
       </div>
